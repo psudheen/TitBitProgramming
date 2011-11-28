7cc11d578387b2e0d8f2639934c98d80242676c3
Pramod Sudheendra <pramodks2006@gmail.com>
2011-11-27 17:46:26 -0800

    Init commit

---
+++ src/C_titbitPorgrams_strManipulate.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : C_titbitPorgrams_strManipulate.c
+ Author      : Pramod Sudheendra
+ Version     :
+ Copyright   : MIT license
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return 1;
+}
